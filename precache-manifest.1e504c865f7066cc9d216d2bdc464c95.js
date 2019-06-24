7641e0c0affdf58170ce0eb8c7bf283ba25028c6
Nick Taber <nick@saleslogistix.com>
2019-06-24 10:52:07 -0700

    Updates

---
+++ precache-manifest.1e504c865f7066cc9d216d2bdc464c95.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd0503f58c68ac50a363d2d3a033d075",
+    "url": "/PvP/index.html"
+  },
+  {
+    "revision": "e96824e813feb98510a4",
+    "url": "/PvP/static/css/2.1d764ec9.chunk.css"
+  },
+  {
+    "revision": "51bea9c20a10f0fe5eb2",
+    "url": "/PvP/static/css/main.bb667c7f.chunk.css"
+  },
+  {
+    "revision": "e96824e813feb98510a4",
+    "url": "/PvP/static/js/2.4955b59b.chunk.js"
+  },
+  {
+    "revision": "51bea9c20a10f0fe5eb2",
+    "url": "/PvP/static/js/main.a255abef.chunk.js"
+  },
+  {
+    "revision": "f5d82d1b22ff919d4ae6",
+    "url": "/PvP/static/js/runtime~main.443f825d.js"
+  },
+  {
+    "revision": "a770b6797b68e3f8920e473eb824bac0",
+    "url": "/PvP/static/media/loader-big.a770b679.gif"
+  },
+  {
+    "revision": "12f0820c451bdc75f4d1ef97732bf6e8",
+    "url": "/PvP/static/media/rw-widgets.12f0820c.woff"
+  },
+  {
+    "revision": "792dcd18baf5f544aabcad1883d673c2",
+    "url": "/PvP/static/media/rw-widgets.792dcd18.svg"
+  },
+  {
+    "revision": "bc7c4a59f924cf037aad6e1f9edba366",
+    "url": "/PvP/static/media/rw-widgets.bc7c4a59.eot"
+  },
+  {
+    "revision": "eceddf474df95d8d4a7e316668c3be85",
+    "url": "/PvP/static/media/rw-widgets.eceddf47.ttf"
+  }
+]);
